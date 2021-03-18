e0fe5273d5bf044a9394ea6341f914a77e191f4e
Ayush Nanda <ananda_be18@thapar.edu>
2021-03-18 14:36:45 +0530

    add

---
+++ todoapp/todoappp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TodoapppConfig(AppConfig):
+    name = 'todoappp'
---
+++ todoapp/todoappp/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def todoview(request):
+    return render(request, 'todoapp/homepage.html')
